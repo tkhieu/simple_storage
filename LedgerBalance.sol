1db3b36bf9a7cc50a053730ef66799e632585c73
Hieu Tran <tr.kimhieu@gmail.com>
2021-12-11 17:15:49 +0700

    Add LedgerBalance to demo mapping

---
+++ LedgerBalance.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.9.0;
+
+contract LedgerBalance {
+   mapping(address => uint) public balances;
+
+   function updateBalance(uint newBalance) public {
+      balances[msg.sender] = newBalance;
+   }
+}
+contract Updater {
+   function updateBalance() public returns (uint) {
+      LedgerBalance ledgerBalance = new LedgerBalance();
+      ledgerBalance.updateBalance(10);
+      return ledgerBalance.balances(address(this));
+   }
+}
